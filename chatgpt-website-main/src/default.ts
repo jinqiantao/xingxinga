02ee475081097c5ed97f5efba41ca8761b0afd5d
jinqiantao <134287306+jinqiantao@users.noreply.github.com>
2023-05-23 11:40:08 +0800

    CHATGPT

---
+++ chatgpt-website-main/src/default.ts
@@ -0,0 +1,13 @@
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  systemRule: ""
+}
+
+export const defaultMessage = `
+- 本站仅用于演示，填入自己的 key 才可使用。
+- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
+- 由 [Chapoii](https://github.com/Chapoii) 修改自 [ourongxing](https://github.com/ourongxing)，查看 [源码](https://github.com/Chapoii/chatgpt-website)，欢迎自部署。
+- **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
